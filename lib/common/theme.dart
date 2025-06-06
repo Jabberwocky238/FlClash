7c49b24b4a5352ef7a5bd9c5c1a4de1ad3b6c419
jabberwocky238 <7176656@qq.com>
2025-06-06 20:47:32 +0800

    dev: combine measure to commontheme

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 class CommonTheme {
   final BuildContext context;
   final Map<String, Color> _colorMap;
+  final Map<String, dynamic> _measureMap;
   final double textScaleFactor;
 
   CommonTheme.of(
     this.context,
     this.textScaleFactor,
-  ) : _colorMap = {};
+  ) : _colorMap = {},
+      _measureMap = {};
 
   Color get darkenSecondaryContainer {
     return _colorMap.updateCacheValue(
@@ -41,6 +43,108 @@ class CommonTheme {
       "darken3PrimaryContainer",
       () => context.colorScheme.primaryContainer
           .blendDarken(context, factor: 0.3),
+    );
+  }
+
+   Size computeTextSize(
+    Text text, {
+    double maxWidth = double.infinity,
+  }) {
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: text.data,
+        style: text.style,
+      ),
+      maxLines: text.maxLines,
+      textScaler: TextScaler.linear(textScaleFactor),
+      textDirection: text.textDirection ?? TextDirection.ltr,
+    )..layout(
+        maxWidth: maxWidth,
+      );
+    return textPainter.size;
+  }
+
+  double get bodyMediumHeight {
+    return _measureMap.updateCacheValue(
+      "bodyMediumHeight",
+      () => computeTextSize(
+        Text(
+          "X",
+          style: context.textTheme.bodyMedium,
+        ),
+      ).height,
+    );
+  }
+
+  double get bodyLargeHeight {
+    return _measureMap.updateCacheValue(
+      "bodyLargeHeight",
+      () => computeTextSize(
+        Text(
+          "X",
+          style: context.textTheme.bodyLarge,
+        ),
+      ).height,
+    );
+  }
+
+  double get bodySmallHeight {
+    return _measureMap.updateCacheValue(
+      "bodySmallHeight",
+      () => computeTextSize(
+        Text(
+          "X",
+          style: context.textTheme.bodySmall,
+        ),
+      ).height,
+    );
+  }
+
+  double get labelSmallHeight {
+    return _measureMap.updateCacheValue(
+      "labelSmallHeight",
+      () => computeTextSize(
+        Text(
+          "X",
+          style: context.textTheme.labelSmall,
+        ),
+      ).height,
+    );
+  }
+
+  double get labelMediumHeight {
+    return _measureMap.updateCacheValue(
+      "labelMediumHeight",
+      () => computeTextSize(
+        Text(
+          "X",
+          style: context.textTheme.labelMedium,
+        ),
+      ).height,
+    );
+  }
+
+  double get titleLargeHeight {
+    return _measureMap.updateCacheValue(
+      "titleLargeHeight",
+      () => computeTextSize(
+        Text(
+          "X",
+          style: context.textTheme.titleLarge,
+        ),
+      ).height,
+    );
+  }
+
+  double get titleMediumHeight {
+    return _measureMap.updateCacheValue(
+      "titleMediumHeight",
+      () => computeTextSize(
+        Text(
+          "X",
+          style: context.textTheme.titleMedium,
+        ),
+      ).height,
     );
   }
 }
