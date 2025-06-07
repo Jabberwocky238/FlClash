bf9d51657100ed5aa382c0dcc5ca3d2f609942cd
jabberwocky238 <7176656@qq.com>
2025-06-07 23:12:56 +0800

    dev: 登陆界面

--- lib/fragments/auth/auth.dart
+++ lib/fragments/auth/auth.dart
@@ -7,6 +7,8 @@ import 'package:jw_clash/state.dart';
 import 'package:jw_clash/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
+import 'functions.dart';
+
 class AuthFragment extends ConsumerStatefulWidget {
   const AuthFragment({super.key});
 
@@ -24,7 +26,7 @@ class _AuthFragmentState extends ConsumerState<AuthFragment> with PageMixin {
     super.initState();
   }
 
-  _initAuthState() {
+  void _initAuthState() {
     final authSetting = ref.watch(authSettingProvider);
     _authStateNotifier.value = authSetting;
   }
@@ -61,15 +63,15 @@ class _AuthFragmentState extends ConsumerState<AuthFragment> with PageMixin {
           padding: baseInfoEdgeInsets,
           child: Column(
             children: [
-              _getUserEmailItem(context, ref),
-              SizedBox(
+              _getUserEmailItem(context),
+              const SizedBox(
                 height: 16,
               ),
-              _getUserPasswordItem(context, ref),
-              SizedBox(
+              _getUserPasswordItem(context),
+              const SizedBox(
                 height: 16,
               ),
-              _getButtonGroup(context, ref),
+              _getButtonGroup(context),
             ],
           ),
         );
@@ -77,121 +79,92 @@ class _AuthFragmentState extends ConsumerState<AuthFragment> with PageMixin {
     );
   }
 
-  Widget _getUserEmailItem(BuildContext context, WidgetRef ref) {
-    final oldEmail =
-        ref.watch(authSettingProvider.select((state) => state.email));
-    return CommonCard(
-      type: CommonCardType.filled,
-      radius: 18,
-      child: ListItem.input(
-        padding: const EdgeInsets.only(
-          left: 16,
-          right: 16,
-          top: 4,
-          bottom: 4,
-        ),
-        title: Text(appLocalizations.userEmail),
-        delegate: InputDelegate(
-          title: appLocalizations.userEmail,
-          value: oldEmail,
-          onChanged: (value) {
-            // ref.read(authSettingProvider.notifier).updateState(
-            //       (state) => state.copyWith(
-            //         email: value ?? oldEmail,
-            //       ),
-            //     );
-            _authStateNotifier.value = _authStateNotifier.value.copyWith(
-              email: value ?? oldEmail,
-            );
-          },
-        ),
-      ),
+  Widget _getUserEmailItem(BuildContext context) {
+    final oldEmail = ref.watch(authSettingProvider.select((state) => state.email));
+    return ValueListenableBuilder<AuthProps>(
+      valueListenable: _authStateNotifier,
+      builder: (context, authState, child) {
+        return CommonCard(
+          type: CommonCardType.filled,
+          radius: 18,
+          child: ListItem.input(
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 16,
+              top: 4,
+              bottom: 4,
+            ),
+            title: Text(appLocalizations.userEmail),
+            delegate: InputDelegate(
+              title: appLocalizations.userEmail,
+              value: authState.email,
+              onChanged: (value) {
+                _authStateNotifier.value = _authStateNotifier.value.copyWith(
+                  email: value ?? oldEmail,
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 
-  Widget _getUserPasswordItem(BuildContext context, WidgetRef ref) {
-    final oldPassword =
-        ref.watch(authSettingProvider.select((state) => state.password));
-    return CommonCard(
-      type: CommonCardType.filled,
-      radius: 18,
-      child: ListItem.input(
-        padding: const EdgeInsets.only(
-          left: 16,
-          right: 16,
-          top: 4,
-          bottom: 4,
-        ),
-        title: Text(appLocalizations.userPassword),
-        delegate: InputDelegate(
-          title: appLocalizations.userPassword,
-          value: oldPassword,
-          onChanged: (value) {
-            // ref.read(authSettingProvider.notifier).updateState(
-            //       (state) => state.copyWith(
-            //         password: value ?? oldPassword,
-            //       ),
-            //     );
-            _authStateNotifier.value = _authStateNotifier.value.copyWith(
-              password: value ?? oldPassword,
-            );
-          },
-        ),
-      ),
+  Widget _getUserPasswordItem(BuildContext context) {
+    final oldPassword = ref.watch(authSettingProvider.select((state) => state.password));
+    return ValueListenableBuilder<AuthProps>(
+      valueListenable: _authStateNotifier,
+      builder: (context, authState, child) {
+        return CommonCard(
+          type: CommonCardType.filled,
+          radius: 18,
+          child: ListItem.input(
+            padding: const EdgeInsets.only(
+              left: 16,
+              right: 16,
+              top: 4,
+              bottom: 4,
+            ),
+            title: Text(appLocalizations.userPassword),
+            delegate: InputDelegate(
+              title: appLocalizations.userPassword,
+              value: authState.password,
+              onChanged: (value) {
+                _authStateNotifier.value = _authStateNotifier.value.copyWith(
+                  password: value ?? oldPassword,
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 
-  Widget _getButtonGroup(BuildContext context, WidgetRef ref) {
-    return CommonChipGroup(
-      chips: [
-        CommonChip(
-          avatar: const Icon(Icons.login),
-          label: appLocalizations.login,
-        ),
-        CommonChip(
-          avatar: const Icon(Icons.person_add),
-          label: appLocalizations.register,
-          onPressed: () => _register(_authStateNotifier.value),
-        ),
-        CommonChip(
-          avatar: const Icon(Icons.logout),
-          label: appLocalizations.logout,
-        ),
-      ],
+  Widget _getButtonGroup(BuildContext context) {
+    return ValueListenableBuilder<AuthProps>(
+      valueListenable: _authStateNotifier,
+      builder: (context, authState, child) {
+        return CommonChipGroup(
+          chips: [
+            CommonChip(
+              avatar: const Icon(Icons.login),
+              label: appLocalizations.login,
+              onPressed: () => login(authState),
+            ),
+            CommonChip(
+              avatar: const Icon(Icons.person_add),
+              label: appLocalizations.register,
+              onPressed: () => register(authState),
+            ),
+            CommonChip(
+              avatar: const Icon(Icons.logout),
+              label: appLocalizations.logout,
+            ),
+          ],
+        );
+      },
     );
   }
 }
 
-Future<void> _register(AuthProps authProps) async {
-  request.post(
-    "$baseUrl/register",
-    {
-      "email": authProps.email,
-      "password": authProps.password,
-    },
-  );
-  final value = await globalState.showCommonDialog<String>(
-    child: InputDialog(
-      title: appLocalizations.pleaseEnterEmailVerificationCode,
-      value: "",
-      suffixText: "",
-      resetValue: "",
-    ),
-  );
-  final response = await request.post(
-    "$baseUrl/verify",
-    {
-      "email": authProps.email,
-      "code": value,
-    },
-  );
-  if (response.statusCode == 200) {
-    globalState.showNotifier(appLocalizations.registerSuccess);
-  }
-  else if (response.statusCode == 450) {
-    globalState.showNotifier(appLocalizations.registerEmailExists);
-  } 
-  else {
-    globalState.showNotifier(appLocalizations.registerFailed);
-  }
-}
